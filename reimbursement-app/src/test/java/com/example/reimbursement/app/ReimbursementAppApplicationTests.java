a89f977d6e6a1d42a2daecbe33131d69dff0c1ab
Rafael <you@example.com>
2024-12-14 16:22:47 -0600

    add reimbursement app

---
+++ reimbursement-app/src/test/java/com/example/reimbursement/app/ReimbursementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reimbursement.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReimbursementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
